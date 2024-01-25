36a32939b65e748c5e4aa87c4d31b4f44ee9c742
Yiwei Liu <2814824250@qq.com>
2024-01-25 18:36:40 +0000

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -84,3 +84,12 @@ function findFriends(
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith("Pa")));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string): string[] {
+  friend.interests
+    ? friend.interests.push(interest)
+    : (friend.interests = [interest]);
+  return friend.interests;
+}
+
+console.log(addInterest(friends[1], "Politics"));
