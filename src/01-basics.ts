3b2885faa65f41a4bd899dbcb5c89953f7a9f1d3
Yiwei Liu <2814824250@qq.com>
2024-01-25 11:23:35 +0000

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -22,7 +22,24 @@ const friends: Friend[] = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
-const colleague1 = {
+
+interface Contact {
+  email: string;
+  extension: number;
+}
+
+interface Colleague {
+  name: string;
+  department: string;
+  contact: Contact;
+}
+
+interface Colleagues {
+  current: Colleague[];
+  former: Colleague[];
+}
+
+const colleague1: Colleague = {
   name: "Ralph Graham",
   department: "Engineering",
   contact: {
@@ -31,7 +48,7 @@ const colleague1 = {
   },
 };
 
-const colleague2 = {
+const colleague2: Colleague = {
   name: "Patti Burke",
   department: "Finance",
   contact: {
@@ -40,7 +57,7 @@ const colleague2 = {
   },
 };
 
-const colleague3 = {
+const colleague3: Colleague = {
   name: "Dean Sullivan",
   department: "HR",
   contact: {
@@ -48,7 +65,8 @@ const colleague3 = {
     extension: 125,
   },
 };
-const colleagues = {
+
+const colleagues: Colleagues = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
