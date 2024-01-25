6161e77da49f31ae6a65f5380ed0e5932af5fbad
Yiwei Liu <2814824250@qq.com>
2024-01-25 19:01:35 +0000

    Type aliases demonstration.

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -1,9 +1,9 @@
 export interface Friend {
-    name: string;
-    phone: string;
-    dob? : Date;   // New
-    age: number;
-    interests? : string[]   // New
+  name: string;
+  phone: string;
+  dob?: Date; // New
+  age: number;
+  interests?: string[]; // New
 }
 
 export interface Colleague {
@@ -24,3 +24,25 @@ export interface EmailContact {
   name: string;
   email: string;
 }
+
+export type Department = "Engineering" | "Finance" | "HR";
+
+export interface ColleagueV2 {
+  name: string;
+  department: Department; // *****
+  contact: {
+    email: string;
+    extension: number;
+    slack?: string;
+  };
+}
+
+export type Buddy = Friend | ColleagueV2;
+
+export type Administrator = Buddy | string | undefined;
+
+export type BuddyList = {
+  name: string;
+  administrator: Administrator;
+  members: Buddy[];
+};
