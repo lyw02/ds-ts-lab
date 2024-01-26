5549241554076620e9368ca787c8d84b23f62de7
Yiwei Liu <2814824250@qq.com>
2024-01-26 13:15:27 +0000

    Functions with generics.

---
+++ src/04-generics.ts
@@ -0,0 +1,25 @@
+import { friends, colleagues } from "./01-basics";
+import { Friend, Colleague } from "./myTypes";
+
+function findMatch<T>(data: T[], criteria: (d: T) => boolean): T | undefined {
+  return data.find(criteria);
+}
+
+console.log(findMatch<Friend>(friends, (f) => f.name.startsWith("Jane")));
+console.log(
+  findMatch<Colleague>(colleagues.current, (c) => c.department === "Finance")
+);
+
+function sort<T>(data: T[], criterion: (a: T, b: T) => number) {
+  return data.sort(criterion);
+}
+
+// Sort friends by age
+console.log(sort<Friend>(friends, (a, b) => a.age - b.age));
+// Sort colleagues by extension number
+console.log(
+  sort<Colleague>(
+    colleagues.current,
+    (a, b) => a.contact.extension - b.contact.extension
+  )
+);
